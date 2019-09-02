4463d5128000d8e3d4b0a10c874e78b6409d3110
elqx <ignasio92@gmail.com>
2019-09-02 12:02:25 +0000

    change data types

--- eloqua/bulk/contacts.go
+++ eloqua/bulk/contacts.go
@@ -14,9 +14,9 @@ type ContactField struct {
 	Name string `json:"name"`
 	InternalName string `json:"internalName"`
 	DataType string `json:"dataType"`
-	HasReadOnlyConstraint string `json:"hasReadOnlyConstrainti,omitempty"`
-	HasNotNullConstraint string `json:"hasNotNullConstraint,omitempty"`
-	HasUniquenessConstraint string `json:"hasUniquenessConstraint,omitempty"`
+	HasReadOnlyConstraint bool `json:"hasReadOnlyConstrainti,omitempty"`
+	HasNotNullConstraint bool `json:"hasNotNullConstraint,omitempty"`
+	HasUniquenessConstraint bool `json:"hasUniquenessConstraint,omitempty"`
 	Statement string `json:"statement"`
 	Uri string `json:"uri"`
 	CreatedAt string `json:"createdAt"`
